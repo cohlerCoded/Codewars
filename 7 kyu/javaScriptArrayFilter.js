32744c5b2026ee1549ab41352e8e92f2a560d23a
Jeff Cohler <jeffcohler@cohlercoded.com>
2021-02-17 11:52:02 -0800

    first commit

---
+++ 7 kyu/javaScriptArrayFilter.js
@@ -0,0 +1,32 @@
+///////////////////////////////PROMPT//////////////////////////////////////////////
+
+/*
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+*/
+
+///////////////////////////////SOLUTION//////////////////////////////////////////////
+
+const getEvenNumbers =(numbersArray) => numbersArray.filter(el => el % 2 === 0)
+
+///////////////////////////////TEST CASES/////////////////////////////////////////
+
+// Since Node 10, we're using Mocha.
+// You can use `chai` for assertions.
+const chai = require("chai");
+const assert = chai.assert;
+// Uncomment the following line to disable truncating failure messages for deep equals, do:
+// chai.config.truncateThreshold = 0;
+// Since Node 12, we no longer include assertions from our deprecated custom test framework by default.
+// Uncomment the following to use the old assertions:
+// const Test = require("@codewars/test-compat");
+
+describe("Solution", function() {
+  it("should test for something", function() {
+    // Test.assertEquals(1 + 1, 2);
+    // assert.strictEqual(1 + 1, 2);
+  });
+});
