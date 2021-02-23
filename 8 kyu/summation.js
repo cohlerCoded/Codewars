4c7bd4166034234e5060ea128ac6b6cf08843a41
Jeff Cohler <jeffcohler@cohlercoded.com>
2021-02-23 14:19:49 -0800

    first commit

---
+++ 8 kyu/summation.js
@@ -0,0 +1,30 @@
+///////////////////////////////PROMPT//////////////////////////////////////////////
+
+/*
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example:
+
+summation(2) -> 3
+1 + 2
+
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+*/
+
+///////////////////////////////SOLUTION//////////////////////////////////////////////
+
+const summation = (num, sum = 0) =>{
+  if (num === 0) return sum
+  sum += num
+  return summation(--num, sum)
+}
+
+///////////////////////////////TEST CASES/////////////////////////////////////////
+
+describe('summation', function () {
+  it('should return the correct total', function () {
+    Test.assertEquals(summation(1), 1)
+    Test.assertEquals(summation(8), 36)
+  })
+})
