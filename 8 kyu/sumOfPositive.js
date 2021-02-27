b0aa0ae0fa694469d0bfb519548e9c2d970530c0
Jeff Cohler <jeffcohler@cohlercoded.com>
2021-02-27 14:47:15 -0800

    Create sumOfPositive.js

---
+++ 8 kyu/sumOfPositive.js
@@ -0,0 +1,21 @@
+///////////////////////////////PROMPT//////////////////////////////////////////////
+
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+///////////////////////////////SOLUTION//////////////////////////////////////////////
+
+const positiveSum = (arr) => arr.reduce((acc, curr) => curr > 0 ? acc += curr: acc += 0, 0)
+
+///////////////////////////////TEST CASES/////////////////////////////////////////
+
+Test.assertEquals(positiveSum([1,2,3,4,5]),15);
+Test.assertEquals(positiveSum([1,-2,3,4,5]),13);
+Test.assertEquals(positiveSum([]),0);
+Test.assertEquals(positiveSum([-1,-2,-3,-4,-5]),0);
+Test.assertEquals(positiveSum([-1,2,3,4,-5]),9);
