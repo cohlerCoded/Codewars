43e75dd838a643c2b7d48ff22c973c7a534ae355
Jeff Cohler <jeffcohler@cohlercoded.com>
2021-03-14 10:43:57 -0700

    Create codingMeetup16HigherOrderFunctionsSeriesAskForMissingDetails.js

---
+++ 6 kyu/codingMeetup16HigherOrderFunctionsSeriesAskForMissingDetails.js
@@ -0,0 +1,75 @@
+///////////////////////////////PROMPT//////////////////////////////////////////////
+
+/*
+You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Given the following input array:
+
+var list1 = [
+  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+write a function that
+
+adds the question property to each object in the input array where the developer has not provided the relevant details (marked with a null value). The value of the question property should be the following string:
+Hi, could you please provide your <property name>.
+
+and returns only the developers with missing details:
+[
+  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java', 
+  question: 'Hi, could you please provide your firstName.' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null, 
+  question: 'Hi, could you please provide your language.' }
+]
+Notes:
+
+At most only one of the values will be null.
+Preserve the order of the original list.
+Return an empty array [] if there is no developer with missing details.
+The input array will always be valid and formatted as in the example above.
+
+*/
+
+///////////////////////////////SOLUTION//////////////////////////////////////////////
+
+function askForMissingDetails(list) {
+  return list.filter(obj =>
+                     Object.values(obj).includes(null)).map(obj =>{
+    for(let key in obj){
+      if (obj[key] === null) obj.question = `Hi, could you please provide your ${key}.`
+    }
+    return obj
+  })
+}
+
+
+///////////////////////////////TEST CASES/////////////////////////////////////////
+
+var list1 = [
+  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+
+var answer1 = [
+  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java', 
+  question: 'Hi, could you please provide your firstName.' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null, 
+  question: 'Hi, could you please provide your language.' }
+];
+
+Test.assertDeepEquals(askForMissingDetails(list1), answer1);
+
+
+var list2 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+
+var answer2 = [];
+
+Test.assertDeepEquals(askForMissingDetails(list2), answer2);
+
+})})
